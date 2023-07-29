c64c453b77722d49eedfeed03b8c15841856dd37
Kyle Fountain <fountainkylec@gmail.com>
2023-07-29 16:14:42 -0400

    Update 2

---
+++ javascript/syntax_comments/syntax_comments.js
@@ -0,0 +1,50 @@
+//COMMENTS
+//This is a single line comment.
+
+/*THis 
+is a multi line comment*/
+
+
+//CODE ETIQUETTE
+//GOOD 
+//Power to be raised
+//5x5=25
+
+//BAD
+//The code below is put in the following order because of x y and z and needs to be multiplied
+//I'm explaining whats going on because you don't have an idea and taking up too much room
+
+//Keep code comments short and to the point.
+//Comments don't show up in the browser or console.
+//------------------------------------------------------------------------
+
+//Javascript statements
+//; separate javascript statements
+
+/* var	Declares a variable
+let 	Declares a block variable
+const 	Declares a block constant
+if	Marks a block of statements to be executed on a condition
+switch	 Marks a block of statements to be executed in different cases
+for	    Marks a block of statements to be executed in a loop
+function	Declares a function
+return	   Exits a function
+try	      Implements error handling to a block of statements */
+
+//Reserved keywords https://www.w3schools.com/js/js_reserved.asp
+
+//---------------------------------------------------------------
+
+//OUTPUT METHODS
+
+//Alert Statement
+alert("hey"); //"alert" javascript statement. This created a popup on the browser.
+
+alert("hey"); alert("hey next"); alert("hey again"); //can include multiple statements on the same line if separated by semicolon. 
+
+alert("error example") //this is the incorrect way to call statements.
+
+alertabc("this is an error"); //this is an incorrect way to use alert statement.
+
+//Console.log Statement
+console.log(); //console.log statement. Sends something to the console.
